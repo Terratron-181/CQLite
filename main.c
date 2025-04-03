de62d373795418d3f060f12b3da6dc1b38ae3183
Terratron-181 <thickbrowncoffee@gmail.com>
2025-04-03 15:50:01 +0200

    better var namings

--- main.c
+++ main.c
@@ -6,25 +6,27 @@
 
 
 
-
+/* ---------- Input buffer interface ----------*/
 typedef struct {
     char* buffer;
-    size_t buffer_length;
+    size_t buffer_size;
     ssize_t input_length;
 } InputBuffer;
 
 InputBuffer* newInputBuffer(void) {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
-    input_buffer->buffer_length = 0;
+    input_buffer->buffer_size = 0;
     input_buffer->input_length = 0;
 
     return input_buffer;
 };
+/* ---------- ---------- ----------*/
 
 
+/* ---------- Open and close the buffer ----------*/
 void readInput(InputBuffer* input_buffer) {
-    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_size), stdin);
 
     if (bytes_read<=0) {
         printf("Error reading input\n");
@@ -39,6 +41,7 @@ void closeInputBuffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
 };
+/* ---------- ---------- ----------*/
 
 
 void printPrompt(void) {
@@ -54,11 +57,35 @@ int main(int argc, char* argv[]) {
         printPrompt();
         readInput(input_buffer);// and saves read data in input_buffer "object";
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            closeInputBuffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        // if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        //     closeInputBuffer(input_buffer);
+        //     exit(EXIT_SUCCESS);
+        // } else {
+        //     printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        // };
+
+        if (input_buffer->buffer[0] == '.') {
+            switch (runMetaCommand(input_buffer)) {
+
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecongized command '%s'\n", input_buffer->buffer);
+                continue;
+            };
         };
+
+        Statement statement;
+        switch (prepareStatement(input_buffer, &statement)) {
+
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+            continue;
+        }
+
+        executeStatement(&statement);
+        printf("Executed.\n");
     };
 }
