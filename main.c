84d559623b0d55c5a6cf920d26d5655717c9d5e9
Terratron-181 <thickbrowncoffee@gmail.com>
2025-04-09 21:19:15 +0200

    compact mem storing functionality

--- main.c
+++ main.c
@@ -2,7 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <sys/types.h>
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+// a hard-coded table
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+#define sizeOfAttribute_macro(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = sizeOfAttribute_macro(Row, id);
+const uint32_t USERNAME_SIZE = sizeOfAttribute_macro(Row, username);
+const uint32_t EMAIL_SIZE = sizeOfAttribute_macro(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+void serializeRow(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE); 
+};
+
+void deserializeRow(void* source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+};
+
+
 
 
 
@@ -59,15 +93,17 @@ typedef enum {
 typedef enum {
     PREPARE_SUCCESS,
     PREPARE_UNRECOGNIZED_STATEMENT
-} PepareResult;
+} PrepareResult;
 
 typedef enum {
     STATEMENT_INSERT,
     STATEMENT_SELECT
 } StatementType;
 
+
 typedef struct {
     StatementType type;
+    Row row_to_insert
 } Statement;
 
 MetaCommandResult runMetaCommand(InputBuffer* input_buffer) {
@@ -79,9 +115,13 @@ MetaCommandResult runMetaCommand(InputBuffer* input_buffer) {
     }
 };
 
-PepareResult prepareStatement(InputBuffer* input_buffer, Statement* statement) {
+PrepareResult prepareStatement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6)==0) {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", $(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+        if (args_assigned < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select")==0) {
