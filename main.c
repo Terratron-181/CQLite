0e6928821cb38f6f047e45eeaa4bc987a97f6357
Terratron-181 <thickbrowncoffee@gmail.com>
2025-04-11 20:27:18 +0200

    Tables layout

--- main.c
+++ main.c
@@ -37,6 +37,28 @@ void deserializeRow(void* source, Row* destination) {
 };
 
 
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+    uint32_t num_of_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if (page==NULL) {
+        // Allocate memory only when we try to access page
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
 
 
 
